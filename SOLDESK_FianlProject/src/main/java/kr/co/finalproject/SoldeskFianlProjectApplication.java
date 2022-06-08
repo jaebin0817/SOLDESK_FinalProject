9080a1b808d03974e6beaa2afd1e25f6ef903bfe
jaebin0817 <jaebin0817@gmail.com>
2022-06-08 23:24:48 +0900

    commit_01

---
+++ SOLDESK_FianlProject/src/main/java/kr/co/finalproject/SoldeskFianlProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoldeskFianlProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoldeskFianlProjectApplication.class, args);
+	}
+
+}
